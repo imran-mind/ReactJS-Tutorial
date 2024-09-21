5ea7ddc5db1be1d2daa662370e13979fbab2249f
imran-mind <shaikhimran115@gmail.com>
2024-09-21 16:31:47 +0530

    05 Class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Profile from './Profile';
 
 function App() {
   return (
     <div className="App">
-
+      Hello World
+      <Profile />
+      <Profile />
+      <Profile />
     </div>
   );
 }
---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Profile extends React.Component {
+
+    render() {
+        return <div>
+            <img src='https://picsum.photos/200/300' alt='profile' />
+        </div>
+    }
+}
+
+export default Profile;
