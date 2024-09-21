1c998bea619399b1319359a48ddd9b3d7f0911ff
imran-mind <shaikhimran115@gmail.com>
2024-09-21 16:13:06 +0530

    React components Code

---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+
+
+function Profile() {
+    return (
+        <div>
+            <h1>John Kanady</h1>
+            <img src="https://picsum.photos/200/300" alt="profile-image" />
+        </div>
+    )
+}
+
+export default Profile;
