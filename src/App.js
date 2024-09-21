16b27f33265715e48b785bf18eca1a47d733462d
imran-mind <shaikhimran115@gmail.com>
2024-09-21 17:00:35 +0530

    Component-Decompostion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
 
+// Navbar
 function App() {
+
+  const Search = () => {
+    const name = "Raj";
+    return <div>
+      <h3>{name}</h3>
+      <input placeholder='search product...' />
+    </div>
+  }
+
+  const Cart = () => {
+    return <div>
+      <h2>Cart Part</h2>
+    </div>
+  }
+
+  const Profile = () => {
+    return <div>
+      <h2>welcome jone</h2>
+    </div>
+  }
+
+  const Other = () => {
+    return <div>
+      <h2>Other part</h2>
+    </div>
+  }
+
+
   return (
     <div className="App">
-
+      <Search />
+      <Cart />
+      <Profile />
+      <Other />
     </div>
   );
 }
