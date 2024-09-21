ee1936d26ecc5e21d6002522778a03e8b520ba55
imran-mind <shaikhimran115@gmail.com>
2024-09-21 16:40:58 +0530

    Component composition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 
+// Navbar
 function App() {
+
+  const Search = () => {
+    return <div>Search Component</div>
+  }
+
+  const Cart = () => {
+    return <div>Cart Component</div>
+  }
+
+  const Profile = () => {
+    return <div>Prifle Component</div>
+  }
+
+  const BecomeASeller = () => {
+    return <div>Become A seller Component</div>
+  }
   return (
     <div className="App">
-
+      <Search />
+      <Cart />
+      <Profile />
+      <BecomeASeller />
     </div>
   );
 }
