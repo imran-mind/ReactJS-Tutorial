a232560bf6afe42347574622d0a4c77fdb5537f3
imran-mind <shaikhimran115@gmail.com>
2024-09-22 12:51:29 +0530

    Uncontrolled component example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const inputRef = useRef(null);
+  const [name, setName] = useState('');
+
+  const handleSubmit = () => {
+    const value = inputRef.current.value.toUpperCase();
+    console.log(value);
+    setName(value);
+  }
   return (
     <div className="App">
+      <h1>Uncontrolled Component</h1>
+      <input
+        type='text'
+        placeholder='Enter your name'
+        ref={inputRef}
+      />
+      <button onClick={handleSubmit}>Submit</button>
 
+      {name && <h1>{name}</h1>}
     </div>
   );
 }
