1e169aeec99c9665eb58f14ebcf706243b10ba1a
imran-mind <shaikhimran115@gmail.com>
2024-09-22 13:20:20 +0530

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isLogin, setIsLogin] = useState(false);
+
+  const Welcome = () => {
+    return <h1>Welcome USer</h1>
+  }
+
+  const Login = () => {
+    return <h1>Login Please</h1>
+  }
+
   return (
     <div className="App">
-
+      <h1>Conditional Rendring in React</h1>
+      {
+        isLogin && <Welcome />
+      }
+      {
+        !isLogin && <Login />
+      }
+      <button onClick={() => setIsLogin(!isLogin)}>Submit</button>
     </div>
   );
 }
