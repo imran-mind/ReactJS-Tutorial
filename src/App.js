1a28e2462edbc05fce909a1b9f41cc10b941f095
imran-mind <shaikhimran115@gmail.com>
2024-09-22 14:54:52 +0530

    React-events

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
 function App() {
+
+  const handleSubmit = (greet, event) => {
+    console.log(event)
+    alert('how are you : ' + greet + " and The event was " + event.type);
+  }
+
+  const handleInputchange = (e) => {
+    console.log(e.target.value);
+    console.log(e.type)
+  }
+  //onclick -> onClick
   return (
     <div className="App">
-
+      <h1>React Events</h1>
+      <input
+        type='text'
+        onChange={handleInputchange}
+      />
+      <button onClick={(event) => handleSubmit("Good morning", event)}>Submit Good morning</button>
+      <button onClick={(event) => handleSubmit("Good Afternoon", event)}>Submit After noon</button>
     </div>
   );
 }
