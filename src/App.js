72880d99db165e3a6f0bd9b495d206f0a8566eab
imran-mind <shaikhimran115@gmail.com>
2024-09-22 16:03:24 +0530

    use Effect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+  useEffect(() => {
+    //subs.connect();
+    // doc.addEvent('click');
+    console.log('use effect hook')
+    document.title = `your count is ${count}`
+    return () => {
+      // cleanup block
+      // subs.disconnect();
+      // doc.remveoEventList
+      console.log('UnMount compn')
+    }
+  }, [count, name])
+  // external api call
+  // dom mani
+  // service subs
   return (
     <div className="App">
-
+      <h1>useEffect()</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(c => c + 1)}>Counter</button>
     </div>
   );
 }
