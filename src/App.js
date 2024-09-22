de342ff888fb5f5c52fb866894a3956ca3ae62b1
imran-mind <shaikhimran115@gmail.com>
2024-09-22 21:42:41 +0530

    useRef example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    // inputRef.current.focus();
+    inputRef.current.select();
+  }, [])
   return (
     <div className="App">
+      <h1>useRef() hook</h1>
 
+      <input
+        type='text'
+        value={"INDIA"}
+        ref={inputRef}
+      />
     </div>
   );
 }
