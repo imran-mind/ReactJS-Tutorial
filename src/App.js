0c3f94f0f782592df31ae53a1c21c23a8b51df13
imran-mind <shaikhimran115@gmail.com>
2024-10-02 15:42:29 +0530

    Reusable component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import './App.css';
 
+function Button({ label, style, onClick }) {
+  return <button
+    className={'button'}
+    style={style}
+    onClick={onClick}
+  >
+    {label}
+  </button>
+}
+
 function App() {
+  const onSave = () => {
+    alert('Save Button Clicked');
+  }
+
+  const onCancle = () => {
+    alert('Cancle Button Clicked');
+  }
+
+  const onReset = () => {
+    alert('Reset Button Clicked');
+  }
+  const saveBtnStyle = {
+    backgroundColor: 'lightgreen'
+  }
+  const cancleBtnStyle = {
+    backgroundColor: 'red',
+    color: 'white'
+  }
+  const resetBtnStyle = {
+    backgroundColor: 'lightblue'
+  }
   return (
     <div className="App">
-
+      <Button
+        style={saveBtnStyle}
+        label={'Save'}
+        onClick={onSave}
+      />
+      <Button
+        label={'Cancle'}
+        onClick={onCancle}
+        style={cancleBtnStyle}
+      />
+      <Button
+        label={'Reset'}
+        onClick={onReset}
+        style={resetBtnStyle}
+      />
     </div>
   );
 }
